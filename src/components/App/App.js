29206cfd8b6b4f9e29880abd057c8025d5dab5cc
Joel Kenneth Fredericks <joel.fredericks@kurtosys.com>
2018-02-12 13:27:07 +0200

    Modified css file, added Github team member names

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import logo from '../../images/logo.svg';
 import './App.css';
+import { GithubCard } from "../githubCard/ghCard";
 
-export class App extends Component {
+/* export class App extends Component {
   render() {
     return (
       <div className="App">
@@ -17,4 +18,23 @@ export class App extends Component {
       </div>
     );
   }
+} */
+
+export class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                    <h2 className="App-sub-title">Kurtosys Grads</h2>
+                </header>
+
+                <div id="gh-card">
+                    <h2>Add Horses</h2>
+                    <GithubCard />
+                </div>
+            </div>
+        )
+    }
 }
---
+++ src/components/githubCard/ghCard.js
@@ -0,0 +1,72 @@
+/* eslint-disable */
+import React from "react";
+import axios from 'axios';
+
+
+const Card = (props) => {
+    return (
+        <div>
+            <img width="75" src={props.avatar_url} alt="avatar image"/>
+            <div>
+                <div>
+                    {props.name}
+                </div>
+                <div>{props.company}</div>
+            </div>
+        </div>
+    );
+};
+
+const CardList = (props) => {
+    return (
+        <div id="cards">
+            {props.cards.map(card => <Card key={card.id} {...card} />)}
+        </div>
+    );
+};
+
+class Form extends React.Component {
+    state = {userName: ''}
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        axios.get(`http://api.github.com/users/${this.state.userName}`)
+        .then(resp => {
+            this.props.onSubmit(resp.data);
+            this.setState({userName: ''});
+        });
+    };
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input type="text"
+                    value={this.state.userName}
+                    onChange={(event) => this.setState({userName: event.target.value})}
+                    placeholder="Github username" required />
+                <button type="submit">Add horse</button>
+            </form>
+        );
+    }
+}
+
+export class GithubCard extends React.Component {
+    state = {
+        cards: []
+    };
+
+    addNewCard = (cardInfo) => {
+        this.setState(prevState => ({
+            cards: prevState.cards.concat(cardInfo)
+        }));
+    };
+
+    render() {
+        return (
+            <div>
+                <Form onSubmit={this.addNewCard} />
+                <CardList cards={this.state.cards} />
+            </div>
+        )
+    }
+}
