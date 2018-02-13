76af175e97db85142e3890cad13ed5e3548dd7f3
Joel Kenneth Fredericks <joel.fredericks@kurtosys.com>
2018-02-13 15:58:17 +0200

    Finalised HTML static framework

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,7 @@
 import React, { Component } from 'react';
 import logo from '../../images/logo.svg';
 import './App.css';
-import { Button } from 'semantic-ui-react';
 //import { GithubCard } from "../githubCard/ghCard";
-
-/* export class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-          <h2 className="App-sub-title">Kurtosys Grads</h2>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/components/App/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-} */
 
 export class App extends Component {
     render() {
@@ -35,15 +17,23 @@ export class App extends Component {
                     <div id="gh-card">
                         <h2>Horse participants</h2>
                         {/* <GithubCard /> */}
-                        <Button></Button>
+                        <button type="submit">Add to Race</button>
                     </div>
 
                     <div id="track">
                         <h2>Race Track</h2>
+                        <button>Start Race</button>
+                        <button type="reset">Reset Race</button>
+                        <div id="grass">
+                            <div id="line-1"></div>
+                            <div id="line-2"></div>
+                            <div id="line-3"></div>
+                        </div>
                     </div>
 
                     <div id="scoreboard">
                         <h2>Scoreboard</h2>
+                        <p>1: Joel</p>
                     </div>
                 </body>
             </div>
